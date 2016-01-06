bff469b2d67826a56eeeda675cec0bf05f5fb891
AWS <aws-mobile-sdk@amazon.com>
2016-01-06 23:07:25 +0000

    AWS SDK for Android 2.2.10

--- src/main/java/com/amazonaws/mobileconnectors/cognito/CognitoSyncManager.java
+++ src/main/java/com/amazonaws/mobileconnectors/cognito/CognitoSyncManager.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2013-2015 Amazon.com,
+ * Copyright 2013-2016 Amazon.com,
  * Inc. or its affiliates. All Rights Reserved.
  *
  * Licensed under the Amazon Software License (the "License").
